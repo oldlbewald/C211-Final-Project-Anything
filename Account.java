7d485930ff82568d6a9a1276855c35b252b93bcd
tylerdel123 <109241349+tylerdel123@users.noreply.github.com>
2022-07-15 21:26:54 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,25 +1,25 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Account {
 	String username;
 	String password;
-	// public Object AccountsFromFile;
 
 	static Scanner scan = new Scanner(System.in);
 
+	// Constructor
 	public Account() {
 		username = "";
 		password = "";
 	}
 
-	public Account(String newusername, String newpassword) {
-		this.username = newusername;
-		this.password = newpassword;
+	// Constructor
+	public Account(String username, String password) {
+		this.username = username;
+		this.password = password;
 	}
-	
+
+	// Method to check if the user-name entered is blank, null or has spaces
 	public static void checkUsername(String username) {
 		if (username != null && !username.isEmpty() && !username.isBlank()) {
 			System.out.println("Username you entered was " + username);
@@ -30,6 +30,7 @@ public class Account {
 		}
 	}
 
+	// Method to check if the password entered is blank, null or has spaces
 	public static void checkPassword(String password) {
 		if (password != null && !password.isEmpty() && !password.isBlank()) {
 			System.out.println("The password you entered was " + password);
@@ -39,34 +40,31 @@ public class Account {
 			checkPassword(password);
 		}
 	}
-	
-	public static ArrayList<Account> readAccountsFromFile(String fileName) throws FileNotFoundException {
-		File file = new File(fileName);
-		Scanner s = new Scanner(file);
 
-		ArrayList<Account> accountlist = new ArrayList<Account>();
+	// Method to check if the user-name & password exist inside of the Accounts.txt
+	// file
+	public static void verifyLogin(String username, String password, String fileName) {
+		boolean found = false;
+		String searchUsername = "";
+		String searchPassword = "";
 
-		// while loop to scan each line
-		while (s.hasNextLine()) {
-			String line = s.nextLine();
+		try {
+			Scanner x = new Scanner(new File(fileName));
+			x.useDelimiter("[ \n]");
 
-			// Finds the separation with the space between username and password
-			String[] items = line.split(" ");
+			while (x.hasNext() && !found) {
+				searchUsername = x.next();
+				searchPassword = x.next();
 
-			// Test to see if its reading the file
-			System.out.println(items[0]);
-			System.out.println(items[1]);
-
-			// Create variables for both items found from file scanner
-			String username = items[0];
-			String password = items[1];
-
-			// create new object with variables
-			Account currentAccounts = new Account(username, password);
-			accountlist.add(currentAccounts);
+				if (searchUsername.trim().equals(username.trim()) && searchPassword.trim().equals(password.trim())) {
+					found = true;
+				}
+			}
+			x.close();
+			System.out.println(found);
+		} catch (Exception e) {
+			System.out.println("Error");
 		}
-
-		s.close();
-		return accountlist;
 	}
+
 }
