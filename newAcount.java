4cc1585197108887cb836d83116122c9c59e25ad
tylerdel123 <109241349+tylerdel123@users.noreply.github.com>
2022-07-15 21:27:40 -0400

    Update newAcount.java

--- newAcount.java
+++ newAcount.java
@@ -5,63 +5,60 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class newAcount {
-String newusername;
-String newpassword;
+	String newusername;
+	String newpassword;
 
-static Scanner scan = new Scanner(System.in);
+	static Scanner scan = new Scanner(System.in);
 
-public newAcount() {
-	newusername = "";
-	newpassword = "";
-}
-public newAcount(String newusername, String newpassword) {
-	this.newusername = newusername;
-	this.newpassword = newpassword;
-}
-
-/*public void createAccount(String newusername, String newpassword)
-{
-	System.out.print("Welcome to Interprise! Lets get you signed up, what is your name? ");
-	newusername = scan.nextLine();
-	
-	System.out.println("Enter a password you would like to use with this account: ");
-	newpassword = scan.next();
-}*/
-
-public static void checkUsername(String newusername) {
-	if (newusername != null && !newusername.isEmpty() && !newusername.isBlank()) {
-		System.out.println("Your username for this account will be " + newusername);
-	} else {
-		System.out.println("The username you entered was empty or not valid. Type it again:");
-		newusername = scan.nextLine();
-		checkUsername(newusername);
-	}
-}
-
-public static void checkPassword(String newpassword) {
-	if (newpassword != null && !newpassword.isEmpty() && !newpassword.isBlank()) {
-		System.out.println("Your password for this account will be " + newpassword);
-		System.out.println("Thanks for creating an account with ");
-	} else {
-		System.out.println("The password you entered was empty or not valid. Type it again:");
-		newpassword = scan.nextLine();
-		checkPassword(newpassword);
-	}
-}
-
-
-public void writeFile(String filename, String newusername, String newpassword)
-{
-	try (FileWriter output = new FileWriter("Accounts.txt", true); 
-		BufferedWriter b = new BufferedWriter(output);
-			PrintWriter p = new PrintWriter(b);)
-	{
-		
-		p.println(newusername + " " + newpassword);
+	// Constructor
+	public newAcount() {
+		newusername = "";
+		newpassword = "";
 	}
 
-	catch (IOException e) {
-		e.printStackTrace();
+	// Constructor
+	public newAcount(String newusername, String newpassword) {
+		this.newusername = newusername;
+		this.newpassword = newpassword;
 	}
-}
+
+	// Method to check if the new user-name entered is blank, null or has spaces
+	public static void checkUsername(String newusername) {
+		if (newusername != null && !newusername.isEmpty() && !newusername.isBlank() && !newusername.contains(" ")) {
+			System.out.println("Your username for this account will be " + newusername);
+		} else {
+			System.out.println(
+					"The username you entered was empty, had spaces or was not a valid username. Type it again:");
+			newusername = scan.nextLine();
+			checkUsername(newusername);
+		}
+	}
+
+	// Method to check if the new password entered is blank, null or has spaces
+	public static void checkPassword(String newpassword) {
+		if (newpassword != null && !newpassword.isEmpty() && !newpassword.isBlank() && !newpassword.contains(" ")) {
+			System.out.println("Your password for this account will be " + newpassword);
+			System.out.println("Thanks for creating an account with Express Rental Service!");
+		} else {
+			System.out.println(
+					"The password you entered was empty, had spaces or was not a valid password. Type it again:");
+			newpassword = scan.nextLine();
+			checkPassword(newpassword);
+		}
+	}
+
+	// Method to create a new account for a user, storing their inputed user-name
+	// and password into the Accounts.txt file on a new line
+	public void writeFile(String filename, String newusername, String newpassword) {
+		try (FileWriter output = new FileWriter("Accounts.txt", true);
+				BufferedWriter b = new BufferedWriter(output);
+				PrintWriter p = new PrintWriter(b);) {
+
+			p.println(newusername + " " + newpassword);
+		}
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
