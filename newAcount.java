727c5e32f10e2f987567f645eb75905ec421e48a
tylerdel123 <109241349+tylerdel123@users.noreply.github.com>
2022-07-17 13:00:45 -0400

    Update newAcount.java

--- newAcount.java
+++ newAcount.java
@@ -1,4 +1,5 @@
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -34,11 +35,47 @@ public class newAcount {
 		}
 	}
 
+	public String checkUsername1(String newusername) {
+		System.out.println("Let's get you signed up");
+		System.out.println("Enter the username you want to use for this account:");
+
+		while (newusername == null || newusername.isEmpty() || newusername.isBlank() || newusername.contains(" ")) {
+			System.out.println("Username was either blank or had spaces. Enter it again:");
+			newusername = scan.nextLine();
+		}
+		// System.out.println("your username will be " + newusername);
+		return newusername;
+	}
+
+	public static void checkUsernameDupe(String newusername, String fileName) {
+		boolean found = false;
+		String searchUsername = "";
+
+		try {
+			Scanner x = new Scanner(new File(fileName));
+			x.useDelimiter("[ \n]");
+
+			while (x.hasNext() && !found) {
+				searchUsername = x.next();
+
+				if (searchUsername.trim().equals(newusername.trim())) {
+					found = true;
+					System.out.println(found);
+				}
+			}
+			x.close();
+			found = false;
+		} catch (Exception e) {
+			System.out.println("username already created");
+		}
+
+	}
+
 	// Method to check if the new password entered is blank, null or has spaces
 	public static void checkPassword(String newpassword) {
 		if (newpassword != null && !newpassword.isEmpty() && !newpassword.isBlank() && !newpassword.contains(" ")) {
 			System.out.println("Your password for this account will be " + newpassword);
-			System.out.println("Thanks for creating an account with Express Rental Service!");
+			System.out.println("Thanks for creating an account with Express Rental Service! Try logging in now.");
 		} else {
 			System.out.println(
 					"The password you entered was empty, had spaces or was not a valid password. Type it again:");
