8d610612dd6571abb9bd822f8ab6d3ef04d4a472
dariancollier <109241013+dariancollier@users.noreply.github.com>
2022-07-18 16:31:45 -0400

    Update Vehicles.java

--- Vehicles.java
+++ Vehicles.java
@@ -1,11 +1,13 @@
 
-
 public class Vehicles {
 	String type;
 	String make;
 	String model;
 	int year;
 	String color;
+        private boolean isRented;       
+
+        
 	
 	//Default Constructor
 		public Vehicles() {
@@ -14,6 +16,7 @@ public class Vehicles {
 			model = "";
 			year = 0;
 			color = "";
+			
 		}
 
 	//Constructor
@@ -23,16 +26,52 @@ public class Vehicles {
 			this.model = model;
 			this.year = year;
 			this.color = color;
+                        this.isRented = isRented;
 		}
 		
 	public static void outputArray(Vehicles[] cars) {
-		for (int i = 0; i < cars.length; i++) {
-			cars[i].output();
-		}
+            for (Vehicles car : cars) {
+                car.output();
+            }
 	}
+
+    Vehicles(String honda, String accord, boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
+        
+       public String getMake() {
+        return make;
+    }
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getName() {
+        return  make +" "+model;
+    }
+
+    public boolean isRented() {
+        return isRented;
+    }
+
+    public void setRented(boolean rented) {
+        isRented = rented;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", isRented=" + isRented +
+                '}';
+    }
 	
 	//Output Method
 		public void output() {
-			System.out.println("" + type + "/" + make + "/" + model + "/" + year + "/" + color);
+			System.out.println("" + carPrice + "/" + type + "/" + make + "/" + model + "/" + year + "/" + color);
 		}
 }
